6027821e3c891989ecfc8446fc58301c66ff26d7
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-01-07 13:39:51 +0100

    + help file

---
+++ t-help.bat
@@ -0,0 +1,25 @@
+@echo off
+
+cls
+
+echo Programming
+	echo  	html template
+	echo			html-template
+	echo		compile C
+	echo			gcc-c file.c
+	echo		compile TS
+	echo			ts-run file.go
+	echo		compile GO
+	echo			go-run file.go
+
+echo.
+
+echo Files
+	echo		edit file
+	echo			edit name.txt
+	echo		view file tree
+	echo			files ./	
+	echo		view file content
+	echo			view file.txt	
+	echo		copy content file
+	echo			context file.txt
